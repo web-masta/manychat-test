42c1d0bc71487ea401fef5151674c3e457c42411
web-masta <ivan.webmasta@gmail.com>
2022-02-10 23:06:28 +0300

    config restructure

--- src/config.php
+++ src/config.php
@@ -4,11 +4,13 @@
 		// Slash if root
 		'base' => '/',
 		
+		'app_name' => 'Organization MVP',
+		
+		// DB settings
 		'db_host' => 'localhost',
 		'db_port' => '',
 		'db_charset' => 'utf8mb4',
 		'db_name' => 'manychattest',
 		'db_user' => 'manychatdbuser',
 		'db_password' => '05o2obuRPS6ubYAAaBu9',
-		'app_name' => 'Organization MVP',
 		];
